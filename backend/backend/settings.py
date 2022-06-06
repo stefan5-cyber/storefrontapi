4c51e51ddca89da80332380874bb1845c9b84938
stefan5-cyber <stefanpetkovic1989@gmail.com>
2022-06-06 23:10:43 +0200

    bugfix: django-debug-toolbar

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,4 +136,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 AUTH_USER_MODEL = 'core.User'
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+}
