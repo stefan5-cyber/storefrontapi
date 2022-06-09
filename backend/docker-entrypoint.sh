24f6c5370ccb7db552fc4561e6a7c1935768dcd2
stefan5-cyber <stefanpetkovic1989@gmail.com>
2022-06-09 11:54:43 +0200

    Make a command to populate database with collections and products

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -6,6 +6,8 @@ echo "Waiting for DB to start..."
 python /backend/manage.py makemigrations
 python /backend/manage.py migrate
 
+python /backend/manage.py seed_db
+
 python /backend/manage.py createsuperuser --noinput
 
 python /backend/manage.py runserver 0.0.0.0:8000
