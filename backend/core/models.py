9ca205f6cb2dcc0ae6a013cf79f33777f845ec6e
stefan5-cyber <stefanpetkovic1989@gmail.com>
2022-06-11 21:48:15 +0200

    Make first and last name required for the registration of users

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -8,3 +9,8 @@ class User(AbstractUser):
     that we can modify it later if necessary
     """
     email = models.EmailField(unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    USERNAME_FIELD = 'username'
